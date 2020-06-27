1143b5c48789657db0606bb31af42af054251f7d
Fabio Spampinato <spampinabio@gmail.com>
2020-06-27 03:17:44 +0100

    Optimized clone function further

--- src/packages/clone.ts
+++ src/packages/clone.ts
@@ -1,13 +1,9 @@
 
-/* IMPORT */
-
-import * as typeOf from 'kind-of';
-
 /* HELPERS */
 
-const {from} = Buffer,
-      {assign, create} = Object,
-      {valueOf} = Symbol.prototype;
+const {assign} = Object;
+
+const CONSTRUCTORS_CLONABLE = new Set ([ Map, Set, Date, RegExp ]);
 
 const cloneMap = ( val: Map<any, any> ): Map<any, any> => {
   const cloned = new Map ();
@@ -33,45 +29,27 @@ const cloneSet = ( val: Set<any> ): Set<any> => {
 
 /* CLONE */
 
-// This is basically a fork of "shallow-clone" but it properly supports Maps, Sets and BigInt TypedArrays
-
 const clone = ( val: any ): any => {
 
-  switch ( typeOf ( val ) ) {
-    case 'array':
-      return val.slice ();
-    case 'object':
-      return assign ( {}, val );
-    case 'date':
-      return new Date ( val.getTime () );
-    case 'map':
-      return cloneMap ( val );
-    case 'set':
-      return cloneSet ( val );
-    case 'buffer':
-      return from ( val );
-    case 'symbol':
-      return Object ( valueOf.call ( val ) );
-    case 'arraybuffer':
-    case 'bigint64array':
-    case 'biguint64array':
-    case 'float32array':
-    case 'float64array':
-    case 'int16array':
-    case 'int32array':
-    case 'int8array':
-    case 'uint16array':
-    case 'uint32array':
-    case 'uint8clampedarray':
-    case 'uint8array':
-      return val.slice ( 0 );
-    case 'regexp':
-      return cloneRegExp ( val );
-    case 'error':
-      return create ( val );
-    default:
-      return val;
+  if ( typeof val !== 'object' || val === null ) return val;
+
+  if ( val.slice ) return val.slice ();
+
+  const {constructor} = val;
+
+  if ( CONSTRUCTORS_CLONABLE.has ( constructor ) ) {
+
+    if ( constructor === Map ) return cloneMap ( val );
+
+    if ( constructor === Set ) return cloneSet ( val );
+
+    if ( constructor === Date ) return new Date ( val.getTime () );
+
+    if ( constructor === RegExp ) return cloneRegExp ( val );
+
   }
+
+  return assign ( {}, val );
 
 };
 
